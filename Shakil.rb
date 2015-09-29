cc955cb16f6a28bf69af1f94891b64ba9ea12a85
Tania Ferman <tania_ferman@hotmail.com>
2015-09-29 21:46:30 +0000

    Shakil the dog

---
+++ Shakil.rb
@@ -0,0 +1,16 @@
+begin
+  puts "say something to Shakil: "
+  word = gets.chomp.downcase
+
+  case word
+    when "woof"
+      puts "WOOF WOOF WOOF"
+    when "shakil stop"  
+      puts
+    when "meow"
+      puts "woof woof woof woof woof"
+    when /treat/  
+    puts  
+  end
+end while word != "go away"
+puts "Shakil has left the building"
